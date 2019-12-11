53f24cbc53db47ef2f0e7cdf3da934c2f829d1d3
Lewis1352 <35689224+Lewis1352@users.noreply.github.com>
2019-12-11 10:51:02 +0000

    Add files via upload
    added second webpage

--- Coursework_Public/index.php
+++ Coursework_Public/index.php
@@ -3,6 +3,20 @@
 	* PHP program to demonstrate the usage of a soap server
 	*/
 
+$make_trace = false;
+
+ini_set('display_errors', 'On');
+ini_set('html_errors', 'On');
+ini_set('xdebug.trace_output_name', 'SoapClient.%t');
+
+if ($make_trace == true && function_exists(xdebug_start_trace()))
+{
+    xdebug_start_trace();
+}
+
 include 'Coursework_Private/bootstrap.php';
 
-
+if ($make_trace == true && function_exists(xdebug_stop_trace()))
+{
+    xdebug_stop_trace();
+}
